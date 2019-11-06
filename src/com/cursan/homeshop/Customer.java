23d82ba5b89a742e60e0b2f93c0213e60e6d90b5
Yacine <Y6879A@GMAIL.COM>
2019-11-06 01:56:28 +0100

    THE SECOND INITIAL TEST

---
+++ src/com/cursan/homeshop/Customer.java
@@ -0,0 +1,14 @@
+package com.cursan.homeshop;
+
+public class Customer {
+    private String fullname;
+    private String address;
+
+    public String getAddress(){
+        return address ;
+    }
+
+    public String getFullname(){
+        return fullname ;
+    }
+}
---
+++ src/com/cursan/homeshop/Product.java
@@ -0,0 +1,37 @@
+package com.cursan.homeshop;
+
+public class Product {
+    private String name;
+    private String description;
+    private double price;
+
+    /**
+     * Display a full description of the product
+     */
+    public void look() {
+        System.out.println(String.format(name + " : " + price + "%n" + description));
+    }
+
+    /**
+     * Add the product to a Bill
+     */
+    public void buy(Bill bill, Integer quantity) {
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
